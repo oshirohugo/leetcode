8979d935a7f97be499422a27eab958724b73f043
Hugo Oshiro <oshiro.hugo@gmail.com>
2023-09-29 11:43:15 +0200

    Improve go solution to string
    Improve go solution to number-of-lines-to-write-string

--- string/number-of-lines-to-write-string/number-of-lines-to-write-string.go
+++ string/number-of-lines-to-write-string/number-of-lines-to-write-string.go
@@ -6,26 +6,22 @@ func numberOfLines(widths []int, s string) []int {
 
 	for i := 0; i < len(s); {
 		try := buffer - widths[s[i]-'a']
-		if try <= 0 {
+
+		if try < 0 {
 			lines++
 			buffer = 100
-			if try == 0 {
-				i++
-			}
 		} else {
 			buffer = try
 			i++
 		}
 	}
 
-	last := buffer
 	if buffer < 100 {
 		lines++
-		last = 100 - last
 	}
 
 	return []int{
 		lines,
-		last,
+		100 - buffer,
 	}
 }
