3773112716a5956a02e885f464255f9f67c88561
Hugo Oshiro <oshiro.hugo@gmail.com>
2023-10-01 20:48:23 +0200

    Add ts solution to string
    Add ts solution to check-if-a-string-is-an-acronym-of-words

---
+++ string/check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.ts
@@ -0,0 +1,13 @@
+function isAcronym(words: string[], s: string): boolean {
+    if (words.length !== s.length) {
+        return false;
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        if (s[i] !== words[i][0]) {
+            return false;
+        }
+    }
+
+    return true;
+};
